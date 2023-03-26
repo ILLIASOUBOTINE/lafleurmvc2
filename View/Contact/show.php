c28bb8850a3c735526531e8868d7ee7a636c323e
ILLIASOUBOTINE <Semenuk1991311@gmail.com>
2023-03-27 00:44:17 +0200

    add page contact map

--- View/Contact/show.php
+++ View/Contact/show.php
@@ -1,12 +1,24 @@
-<section class="details">
+<section class="contact">
 
-    <div class="details_produit">
-        <h1>Contact</h1>
+    <div class="block_contact">
+        <p class="pd24w600">Adresse:</p>
+        <p class="int20w400">7 Rue du Grand Pré, 84160 Lourmarin</p>
 
+        <iframe class="google_map"
+            src="
+            https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2881.4051008309025!2d5.361436015779903!3d43.764449053247255!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12ca18e8be25e425%3A0x17625c3b5babd743!2s7%20Rue%20du%20Grand%20Pr%C3%A9%2C%2084160%20Lourmarin!5e0!3m2!1sfr!2sfr!4v1679864184544!5m2!1sfr!2sfr"
+            allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade">
+        </iframe>
     </div>
 
-    <div class="details_produit">
-        <h1>Contact</h1>
+    <div class="block_contact">
+        <p class="pd24w600">Adresse:</p>
+        <p class="int20w400">7 Rue du Grand Pré, 84160 Lourmarin</p>
 
+        <iframe class="google_map"
+            src="
+            https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2881.4051008309025!2d5.361436015779903!3d43.764449053247255!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12ca18e8be25e425%3A0x17625c3b5babd743!2s7%20Rue%20du%20Grand%20Pr%C3%A9%2C%2084160%20Lourmarin!5e0!3m2!1sfr!2sfr!4v1679864184544!5m2!1sfr!2sfr"
+            allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade">
+        </iframe>
     </div>
 </section>
